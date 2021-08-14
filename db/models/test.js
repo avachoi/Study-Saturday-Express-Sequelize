c2cfedbf504dd05769e4df3efe93fe694e3e951e
Ava Choi <alexbchoi@gmail.com>
2021-08-14 11:57:58 -0400

    cycle2

--- db/models/test.js
+++ db/models/test.js
@@ -1,7 +1,21 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
-const Student = require('./student');
+const Sequelize = require("sequelize");
+const db = require("../db");
+const Student = require("./student");
 
-const Test = db.define();
+const Test = db.define("test", {
+	subject: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	grade: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+	},
+});
+
+// Test.belongsTo(Student);
+// Student.hasOne(Test);
+
+Test.belongsTo(Student, { as: "student" });
 
 module.exports = Test;
