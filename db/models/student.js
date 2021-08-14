58f446bf4584ea6e6f13bb3ad5cddd63987a2c18
Ava Choi <alexbchoi@gmail.com>
2021-08-14 11:16:30 -0400

    cycle1

--- db/models/student.js
+++ db/models/student.js
@@ -1,6 +1,31 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const Student = db.define();
+const Student = db.define("student", {
+	firstName: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	lastName: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	email: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			isEmail: true,
+		},
+	},
+});
+
+Student.addHook("beforeCreate", (studentInstance, optionsObject) => {
+	studentInstance.firstName =
+		studentInstance.firstName[0].toUpperCase() +
+		studentInstance.firstName.slice(1);
+	studentInstance.lastName =
+		studentInstance.lastName[0].toUpperCase() +
+		studentInstance.lastName.slice(1);
+});
 
 module.exports = Student;
